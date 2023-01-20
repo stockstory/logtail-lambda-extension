df959a0f79f6801ac8e4d1fee3afbe0bbf493dc1
jabin Bastian <this@jabin.id.au>
2023-01-20 13:38:01 +1100

    Add support for aws-lambda-powertools-typescript structured logs

--- src/forwarders/logtail.ts
+++ src/forwarders/logtail.ts
@@ -1,6 +1,39 @@
 import fetch from 'node-fetch';
 import { FunctionLogEvent } from '~/aws/events';
-import { function as F, array as A } from 'fp-ts';
+import { function as F, array as A, either as E } from 'fp-ts';
+import { z } from 'zod';
+
+// @see https://awslabs.github.io/aws-lambda-powertools-typescript/latest/core/logger/#standard-structured-keys
+export const powertoolsLogSchema = z
+  .object({
+    level: z.string(), // Logging level set for the Lambda function's invocation
+    message: z.string(), // A descriptive, human-readable representation of this log item
+    timestamp: z.string(), // Timestamp string in simplified extended ISO format (ISO 8601)
+    service: z.string(), // A unique name identifier of the service this Lambda function belongs to, by default service_undefined
+    xray_trace_id: z.string().optional(), // X-Ray Trace ID. Always present in Lambda environment, but not sent locally.
+    sampling_rate: z.number().optional(), // When enabled, it prints all the logs of a percentage of invocations, e.g. 10%
+    error: z.string().optional(), // Optional - An object containing information about the Error passed to the logger
+  })
+  .passthrough();
+
+export type PowertoolsLogRecord = z.infer<typeof powertoolsLogSchema>;
+
+export const parseMessageWithPowertoolsLogFormat = (message: string): E.Either<Error, PowertoolsLogRecord> =>
+  F.pipe(
+    E.tryCatch(
+      (): unknown => JSON.parse(message),
+      (reason) => new Error(`String is not JSON: ${reason}`),
+    ),
+    E.chain((entry) => {
+      const result = powertoolsLogSchema.safeParse(entry);
+
+      if (result.success) {
+        return E.right(result.data);
+      } else {
+        return E.left(new Error(`Message is not in lambda powertools format`));
+      }
+    }),
+  );
 
 export const logtailLogForwarder =
   (token: string, ingestionUrl: string, listener: { logsQueue: FunctionLogEvent[] }) => (): Promise<void> => {
@@ -15,10 +48,23 @@ export const logtailLogForwarder =
       body: JSON.stringify(
         F.pipe(
           logs,
-          A.map((log) => ({
-            dt: log.time,
-            message: log.record,
-          })),
+          A.map((log) =>
+            F.pipe(
+              log.record,
+              parseMessageWithPowertoolsLogFormat,
+              E.fold(
+                () => ({
+                  dt: log.time,
+                  message: log.record,
+                }),
+                ({ message, ...data }) => ({
+                  dt: log.time,
+                  message,
+                  data,
+                }),
+              ),
+            ),
+          ),
         ),
       ),
       headers: {
